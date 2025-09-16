ae485d11c32dd15f8999eea731125e6867d1899f
yyxff <salmontwosday@gmail.com>
2025-09-16 12:28:38 -0400

    init project

---
+++ src/main/java/io/github/yyxff/stockarena/StockArenaApplication.java
@@ -0,0 +1,13 @@
+package io.github.yyxff.stockarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockArenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockArenaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/yyxff/stockarena/StockArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yyxff.stockarena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockArenaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
